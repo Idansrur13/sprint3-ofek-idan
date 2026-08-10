0d86ead5a83456f8027e4c4ec51d6a29cff5840c
Ofek Meshulam <ofekmeshulam46@gmail.com>
2026-08-11 02:28:35 +0300

    Mail Info Organized From Data

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,13 +1,24 @@
+import { utilService } from "../../../services/util.service.js"
 
-export function MailList({mails}) {
-    return <div>
-        {mails.map((mail) => (
+export function MailList({ mails }) {
+  return (
+    <div>
+      {mails.map((mail) => (
         <div key={mail.id}>
-          <h2>{mail.id}</h2>
+          <h2>from {mail.from} </h2>
+          <h2>{mail.subject} </h2>
+          <p>{mail.id}</p>
           <p>{mail.createdAt}</p>
-          <p>{mail.subject}</p>
-          {/* <p>{utilService.getMonthName(${mail.createdAt})}</p> */}
+          <p>{mail.body}</p>
+          <span>
+            {utilService.getNumericDate(mail.createdAt,'he-IL')}-
+            {utilService.getYearName(mail.createdAt)}-
+            {utilService.getMonthName(mail.createdAt)}-
+            {utilService.getDayName(mail.createdAt)}-
+            {utilService.getTimeOfDay(mail.createdAt)}
+          </span>
         </div>
       ))}
     </div>
+  )
 }
--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -1,13 +1,10 @@
 const { useState, useEffect } = React
 import { demoMails } from "../services/demoDataMail.jsx"
-import { utilService } from "../../../services/util.service.js"
 import { MailList } from "../cmps/MailList.jsx"
 
 
 export function MailIndex() {
   console.log(demoMails)
-  const t=utilService.getRandomColor()
-  console.log({t})
   const mails= demoMails
 
   return (
