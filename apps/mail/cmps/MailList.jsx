9e68a31c32ffecabd49aaebe7c728e7891fff1a7
Ofek Meshulam <ofekmeshulam46@gmail.com>
2026-08-11 00:43:45 +0300

    Gmail- connect index and list to demodata

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,3 +1,13 @@
-export function MailList() {
-    return <div>Mail list</div>
+
+export function MailList({mails}) {
+    return <div>
+        {mails.map((mail) => (
+        <div key={mail.id}>
+          <h2>{mail.id}</h2>
+          <p>{mail.createdAt}</p>
+          <p>{mail.subject}</p>
+          {/* <p>{utilService.getMonthName(${mail.createdAt})}</p> */}
+        </div>
+      ))}
+    </div>
 }
--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -1,5 +1,18 @@
+const { useState, useEffect } = React
+import { demoMails } from "../services/demoDataMail.jsx"
+import { utilService } from "../../../services/util.service.js"
+import { MailList } from "../cmps/MailList.jsx"
+
 
 export function MailIndex() {
-    return <section className="container">Mail app</section>
-}
+  console.log(demoMails)
+  const t=utilService.getRandomColor()
+  console.log({t})
+  const mails= demoMails
 
+  return (
+    <section className="container">
+      <MailList mails={mails} />
+    </section>
+  )
+}
