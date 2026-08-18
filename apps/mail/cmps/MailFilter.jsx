c95ca968de39a9b6c0aee472a402b3de59b241af
Ofek Meshulam <ofekmeshulam46@gmail.com>
2026-08-18 20:22:20 +0300

    basic fixes

--- apps/mail/cmps/MailFilter.jsx
+++ apps/mail/cmps/MailFilter.jsx
@@ -1,6 +1,6 @@
 const { useState, useEffect } = React
-import { useEffectUpdate } from '../custom-hooks/useEffectUpdate.js'
-import { emailsService } from '../services/mail.service.js'
+import { useEffectUpdate } from "../custom-hooks/useEffectUpdate.js"
+import { emailsService } from "../services/mail.service.js"
 
 export function MailFilter({ filterBy, onSetFilterBy }) {
   const [filterByToEdit, setFilterByToEdit] = useState(filterBy)
@@ -22,30 +22,30 @@ export function MailFilter({ filterBy, onSetFilterBy }) {
     setFilterByToEdit(mailService.getDefaultFilter())
   }
   return (
-    <section className='mail-filter'>
-      <p>
+    <section className="mail-filter">
+      <span>
         <svg
-          xmlns='http://www.w3.org/2000/svg'
-          width='24'
-          height='24'
-          viewBox='0 0 24 24'
-          fill='none'
-          stroke='currentColor'
-          stroke-width='2'
-          stroke-linecap='round'
-          stroke-linejoin='round'
-          class='lucide lucide-search-icon lucide-search'
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="lucide lucide-search-icon lucide-search"
         >
-          <path d='m21 21-4.34-4.34' />
-          <circle cx='11' cy='11' r='8' />
+          <path d="m21 21-4.34-4.34" />
+          <circle cx="11" cy="11" r="8" />
         </svg>
-      </p>
+      </span>
       <input
         onChange={(ev) => handleChange(ev)}
         value={filterByToEdit.txt}
-        type='text'
-        name='txt'
-        placeholder='Search mail'
+        type="text"
+        name="txt"
+        placeholder="Search mail"
       />
       {/* <button></button> add serach options emoji */}
     </section>
