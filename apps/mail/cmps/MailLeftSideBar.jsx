56781c36c83b4801353ea11a29a6add0aa9c1ab9
Ofek Meshulam <ofekmeshulam46@gmail.com>
2026-08-18 07:26:43 +0300

    fix compose link

--- apps/mail/cmps/MailLeftSideBar.jsx
+++ apps/mail/cmps/MailLeftSideBar.jsx
@@ -1,16 +1,14 @@
 const { Link } = ReactRouterDOM
 import { icons } from "../services/mail.icons.js"
-export function MailLeftSideBar({unreadMailCount }) {
+export function MailLeftSideBar({ unreadMailCount }) {
   console.log("readMailCount:", unreadMailCount)
   return (
     <section className="mail-left">
       <Link to="/mail/edit">
         <button>{icons.compose}</button>
-        <button>
-          {icons.sent}
-        </button>
-            <div>Unread Mails:{unreadMailCount}</div>
       </Link>
+      <button>{icons.sent}</button>
+      <div>Unread Mails:{unreadMailCount}</div>
     </section>
   )
 }
