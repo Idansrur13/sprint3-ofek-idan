a21e5bd6aa16a50a06677e280c9ee99933d67340
Ofek Meshulam <ofekmeshulam46@gmail.com>
2026-08-18 19:38:20 +0300

    icons folder

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,16 +1,56 @@
-const { Link, NavLink } = ReactRouterDOM
+const { Link, NavLink, useLocation } = ReactRouterDOM
+const { useState } = React
+import { Menu, MailIcon, KeepIcon } from "../apps/icons/icons.jsx"
+
+const APP_BRANDS = {
+  "/mail": { Icon: MailIcon, label: "GMAIL" },
+  "/note": { Icon: KeepIcon, label: "KEEP" },
+}
+const DEFAULT_BRAND = { Icon: KeepIcon, label: "APPSUS" }
 
 export function AppHeader() {
-
-    return <header className="app-header">
-        <Link to="/">
-            <h3>LOGO ✨</h3>
-        </Link>
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
-        </nav>
+  const { pathname } = useLocation()
+  const brandKey = Object.keys(APP_BRANDS).find((path) =>
+    pathname.startsWith(path),
+  )
+  const [openNavBar, setOpenNavBar] = useState(false)
+  const { Icon, label } = APP_BRANDS[brandKey] || DEFAULT_BRAND
+  console.log(pathname, "patttha nameeeeeeeeeeee")
+  return (
+    <header className="app-header ">
+      <div onClick={() => setOpenNavBar((v) => !v)}>
+        <Menu />
+      </div>
+      <Link to="/">
+        <div
+          style={{ display: "flex", height: 28, gap: 4, alignItems: "center" }}
+        >
+          <Icon />
+          <h4>{label}</h4>
+        </div>
+      </Link>
+      {openNavBar && (
+        <div style={{ position: "absolute", top: 40, zIndex: 4 }}>
+          <NavBar label={label} />
+        </div>
+      )}
     </header>
+  )
+}
+
+function NavBar({ label }) {
+  return (
+    <nav className="nav-div">
+      <NavLink to="/"> Home</NavLink>
+      <NavLink to="/about">About</NavLink>
+      <NavLink to="/mail">
+        <MailIcon /> Mail
+      </NavLink>
+      <NavLink to="/note">
+        {" "}
+        <KeepIcon />
+        Note
+      </NavLink>
+    </nav>
+  )
 }
